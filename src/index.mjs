af1b009a978ade2b63494b062d9080004da2d4a7
AbijuruT <abijuruyves@gmail.com>
2024-07-23 20:24:49 +0200

    Getting hang out with routes or endpoints with get

--- src/index.mjs
+++ src/index.mjs
@@ -9,13 +9,20 @@ app.get("/", (req, response) => {
 app.get("/api/users", (request, response) => {
   response.send([
     { id: 1, username: "Thor", displayName: "Yves" },
-    { id: 1, username: "Assia", displayName: "Massiah" },
-    { id: 1, username: "Thor", displayName: "Yves" },
-    { id: 1, username: "Thor", displayName: "Yves" },
-    { id: 1, username: "Thor", displayName: "Yves" },
+    { id: 2, username: "Assia", displayName: "Massiah" },
+    { id: 3, username: "Esther", displayName: "Hommy" },
+    { id: 4, username: "Thorine", displayName: "lizzy" },
+    { id: 5, username: "Locky", displayName: "wizard" },
   ]);
 });
 
+app.get('/api/products', (request, response) => {
+  response.send([
+    { id: 1, name: "Chiken", price: 12.99 },
+    { id: 2, name: "Chips", price: 499.9 },
+    { id: 3, name: "Burger", price: 12.79 },
+  ]);
+});
 app.listen(PORT, () => {
   console.log(`Running on port ${PORT}`);
 });

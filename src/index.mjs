369e9fbe8d6fef7123731574c42e11f4c1e05eb3
AbijuruT <abijuruyves@gmail.com>
2024-07-23 20:09:35 +0200

    Getting hang out with routes or endpoints with get

--- src/index.mjs
+++ src/index.mjs
@@ -3,6 +3,18 @@ import express from "express";
 const app = express();
 
 const PORT = process.env.PORT || 5500;
+app.get("/", (req, response) => {
+  response.status(201).send({ msg: "Helloo world" });
+});
+app.get("/api/users", (request, response) => {
+  response.send([
+    { id: 1, username: "Thor", displayName: "Yves" },
+    { id: 1, username: "Assia", displayName: "Massiah" },
+    { id: 1, username: "Thor", displayName: "Yves" },
+    { id: 1, username: "Thor", displayName: "Yves" },
+    { id: 1, username: "Thor", displayName: "Yves" },
+  ]);
+});
 
 app.listen(PORT, () => {
   console.log(`Running on port ${PORT}`);
